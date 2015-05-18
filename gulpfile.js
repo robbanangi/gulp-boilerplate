62e64912a9c53cb50b9cccc495b8f7c0d45bc93b
robbanangi <robert.angi95@gmail.com>
2015-05-18 16:18:36 +0200

    Initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,60 @@
-//* Variables and npm installs 
+// Npm installs and properties //
 
 var gulp = require('gulp'),
 jade = require('gulp-jade'),
 sass = require('gulp-sass'),
-concat = require('gulp-concat'),
 browserSync = require('browser-sync'),
 plumber = require('gulp-plumber'),
-reload = browserSync.reload;
+concat = require('gulp-concat'),
+imagemin = require('gulp-imagemin'),
+reload = browserSync.reload,
+uglify = require('gulp-uglify');
 
-//* Gulp tasks for different properties *//
-		gulp.task('browser-sync', function(){
-			browserSync({
 
-				server: {
-					baseDir: '_site'
-				}
-			});
+// Gulp tasks for our Npm installs and properties! //
+
+
+		gulp.task('browser-sync', function() {
+    			browserSync ({
+        				server: {
+            		baseDir: "_site"
+        		}
+    		});
 		});
-	
+
 
 		gulp.task('jade', function(){
 				return gulp.src('src/views/*.jade')
-				.pipe(plumber()) // Very important //
+				.pipe(plumber())
 				.pipe(jade())
-				.pipe(plumber.stop()) // Very important //
+				.pipe(plumber.stop())
 				.pipe(gulp.dest('_site'))
 				.pipe(reload({stream:true}))
-			});
+		});
 
 		gulp.task('sass', function(){
 				return gulp.src('src/scss/**/*.scss')
-				.pipe(plumber()) // Very important //
+				.pipe(plumber())
 				.pipe(sass())
-				.pipe(plumber.stop()) // Very important //
+				.pipe(plumber.stop())
 				.pipe(gulp.dest('_site/assets/css/'))
 				.pipe(reload({stream:true}))
-			});
-
-		gulp.task('script', function() {
-			return gulp.src('src/js/*.js')
-			.pipe(plumber()) // Very important //
-			.pipe(concat('script.js'))
-			.pipe(plumber.stop()) // Very important //
-			.pipe(gulp.dest('_site/assets/js/'))
-		
 		});
 
 
+		gulp.task('concat', function(){
+				return gulp.src('src/js/*.js')
+				.pipe(concat('script.js'))
+				.pipe(uglify())
+				.pipe(gulp.dest('_site/assets/js/'))
+		});
+
 		gulp.task('watch', function() {
-
-				gulp.watch('src/views/**/*.jade', ['jade']) // ( Very important ) //
+				gulp.watch('src/views/*.jade', ['jade'])
 				gulp.watch('src/scss/**/*.scss', ['sass'])
-				gulp.watch('src/js/*.js', ['script'])
+				gulp.watch('src/js/*.js', ['concat'])
+		});
 
-			});
+		gulp.task('default', ['jade', 'sass', 'concat', 'watch', 'browser-sync'], function(){
 
-		gulp.task('default', ['browser-sync', 'jade', 'sass','script', 'watch'], function() {
-
-			});
+		});
